223613e886b58a9f15bf3ed4d37903f6e50f6073
I_AM_TINE <sutichok46@gmail.com>
2021-10-06 16:26:57 +0700

    P Gun

--- react/src/_profile/components/ProfileSideBar.js
+++ react/src/_profile/components/ProfileSideBar.js
@@ -1,6 +1,6 @@
 import {Avatar, Box,Stack, Typography} from '@mui/material'
 const ProfileSideBar = ()=>{
-    const profilepic = `https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/241840764_1946278805560244_4803799378012841887_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeG6UPNmZltUQkt6Y2q_ZZ6bouSsw4HwK6qi5KzDgfArqjfhnJYb8vpJ4vQLbar3DQ9WFSQ0uKFFSz3H-Ej5imPf&_nc_ohc=7Y5Jtj85rCwAX_WVaXj&tn=urA0xrjws_8evN7x&_nc_ht=scontent.fbkk5-3.fna&oh=ddc6f358fc3c4c84d3e6ad4200d4f3b9&oe=61615048`
+    const profilepic = `https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/69999079_867212440345352_2645553958591922176_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFIaT0-vKeZl-tKtA_G1ssLxZ-ZDAFHbXLFn5kMAUdtcodrIQfzlKEkFdqi78Yh6tzb4lLjgyPJAzoXSVDqeHOE&_nc_ohc=_mc4q0_1Es0AX9588rq&_nc_ht=scontent.fbkk5-6.fna&oh=d594ca4d3f1d283e127e27a5cb69e828&oe=6183ABA9`
     return(
         <Box >
             <Stack gap={3} mt={5}>
--- react/src/utils/axios.js
+++ react/src/utils/axios.js
@@ -2,8 +2,8 @@ import instance from 'axios';
 
 const getBaseURL = () => {
 	return (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')
-		? 'http://localhost:1337'
-		: 'https://dp-nskmutt-strapi.bsthun.com';
+		? 'https://dp-nxkmutt-strapi.bsthun.com'
+		: 'https://dp-nxkmutt-strapi.bsthun.com';
 };
 
 const axios = instance.create({
