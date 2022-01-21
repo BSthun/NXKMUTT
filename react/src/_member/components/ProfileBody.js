3a6b20626f84bf718623e2c4e091c1d6c6d911d5
Thun <bsthun@gmail.com>
2022-01-22 03:30:34 +0700

    Fix warnings

--- react/src/_member/components/ProfileBody.js
+++ react/src/_member/components/ProfileBody.js
@@ -1,64 +1,67 @@
 import {
-	faFacebook,
-	faTwitter,
-} from '@fortawesome/free-brands-svg-icons';
-import {
-	faAt,
-	faPhone,
-} from '@fortawesome/free-solid-svg-icons';
-import {
 	Box,
-	Card,
 	Grid,
-	Stack,
 	Typography,
 } from '@mui/material';
 import React from 'react';
-import BlogItem from '../../_content/components/BlogItem';
-import ContactIcon from './ContactIcon';
-import ContentChip from './ContentChip';
-import { strapiAxios } from '../../utils/axios';
 import { useTranslation } from 'react-i18next';
+import BlogItem from '../../_content/components/BlogItem';
+import { strapiAxios } from '../../utils/axios';
 import ProfileSection from './ProfileSection';
 
 const ProfileBody = ({ member }) => {
 	const [, i18n] = useTranslation('home');
-	console.log("member",member)
+	console.log('member', member);
 	const publications = member.attributes.publications.data;
 	return (
 		<Box minHeight="80vh" flex={1}>
-			<Grid spacing={5} container sx={{padding: '0rem 3rem'}}>
+			<Grid spacing={5} container sx={{ padding: '0rem 3rem' }}>
 				<Grid item md={3}>
-					<Box sx={{width: '100%', aspectRatio: "1", overflow: 'hidden', borderRadius: '100%', transform: 'scale(.8)'}}>
-						<img style={{width: '100%'}} src={strapiAxios.baseURL + member.attributes.photo?.data.attributes.url} alt={member.name} />
+					<Box sx={{
+						width: '100%',
+						aspectRatio: '1',
+						overflow: 'hidden',
+						borderRadius: '100%',
+						transform: 'scale(.8)',
+					}}
+					>
+						<img style={{ width: '100%' }}
+						     src={strapiAxios.baseURL + member.attributes.photo?.data.attributes.url}
+						     alt={member.name}
+						/>
 					</Box>
 				</Grid>
-				<Grid item md={9} sx={{alignItems: 'center', display: "flex", flexWrap:'wrap'}}>
-					<Box sx={{ width: '100%'}}>
-						<Typography variant='h3' color="textPrimary">{member.attributes[`name_${i18n.language}`]} {member.attributes[`surname_${i18n.language}`]}</Typography>
-						<Typography variant='h6' color="textPrimary" sx={{fontWeight: '400'}}>{member.attributes[`position`]}</Typography>
+				<Grid item md={9} sx={{ alignItems: 'center', display: 'flex', flexWrap: 'wrap' }}>
+					<Box sx={{ width: '100%' }}>
+						<Typography variant="h3"
+						            color="textPrimary"
+						>{member.attributes[`name_${i18n.language}`]} {member.attributes[`surname_${i18n.language}`]}</Typography>
+						<Typography variant="h6"
+						            color="textPrimary"
+						            sx={{ fontWeight: '400' }}
+						>{member.attributes[`position`]}</Typography>
 					</Box>
 				</Grid>
 			</Grid>
-			<Grid container spacing={5}  mt={1}>
+			<Grid container spacing={5} mt={1}>
 				<Grid item md={6}>
-					<ProfileSection title="Contact details" member={member}/>
+					<ProfileSection title="sContact details" member={member} />
 				</Grid>
 				<Grid item md={6}>
-					<ProfileSection title="Attributes" member={member} attributes/>
+					<ProfileSection title="Attributes" member={member} attributes />
 				</Grid>
 			</Grid>
 			<Grid spacing={5} container mt={5}>
-				{publications.map(({attributes: item}) => 
+				{publications.map(({ attributes: item }) =>
 					<Grid key={item} item xs={4}>
 						<BlogItem title={item.title}
-							description={item.desc}
-							date={item.published}
-							background={strapiAxios.baseURL + item.banner?.data.attributes.url}
-							height
-							to={`/publication/${item.slug}`}
+						          description={item.desc}
+						          date={item.published}
+						          background={strapiAxios.baseURL + item.banner?.data.attributes.url}
+						          height
+						          to={`/publication/${item.slug}`}
 						/>
-					</Grid>
+					</Grid>,
 				)}
 			</Grid>
 		</Box>
--- react/src/_member/components/ProfileSection.js
+++ react/src/_member/components/ProfileSection.js
@@ -1,72 +1,122 @@
-import { faFacebook, faInstagram, faLine, faLinkedin, faTelegram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import {
+	faFacebook,
+	faInstagram,
+	faLine,
+	faLinkedin,
+	faTelegram,
+	faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { faGlobe, faPhone } from '@fortawesome/free-solid-svg-icons';
+import {
+	faGlobe,
+	faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Card, CardContent, CardHeader, Stack, Typography, Box } from '@mui/material';
-import { useState } from 'react';
+import {
+	Box,
+	Card,
+	CardContent,
+	Stack,
+	Typography,
+} from '@mui/material';
 
-
-const ProfileSection = ({ title="{{section_title}}", member, attributes=false}) => {
-    const emails = member.attributes.emails;
-    const phones = member.attributes.phones;
-    const attrs = member.attributes.attrs;
-    const socials = member.attributes.socials;
-    const socialIcons = {
-        facebook: faFacebook,
-        twitter: faTwitter,
-        telegram: faTelegram,
-        line: faLine,
-        instagram: faInstagram,
-        linkedin: faLinkedin,
-        web: faGlobe
-    };
-    
-    return <Card  variant="outlined" sx={{height: '100%'}}>
-        <CardContent>
-            
-            <Typography variant='h6'  component="div" color="textPrimary" fontWeight="600">
-                {title}
-            </Typography>
-            { 
-                attributes ? 
-                <Stack direction={"column"} gap={2}>
-                    {attrs.map(({name, value}, index) => <Typography variant='p'  component="div" color="textPrimary">
-                            <Box>
-                                <Typography variant='b' component="b">{name}</Typography>
-                                <Typography variant='body2' component="div" dangerouslySetInnerHTML={{__html: value.split('\n').join('<br>')}}></Typography>
-                            </Box>
-                        </Typography>)}
-                </Stack> 
-                : 
-                <Stack direction={"column"} gap={2}>
-                    {/* Email */}
-                    <Box>
-                        {emails.map(({email, title}, index) => <Typography variant='p'  component="div" color="textPrimary">
-                            <FontAwesomeIcon icon={faEnvelope} style={{marginRight: '.25rem', opacity: index > 0 ? 0 : 1}}/> {email} { emails.length > 1 && <Typography variant="b" component="span" color="textPrimary" style={{opacity: '.5'}}>• {title}</Typography>}
-                        </Typography>)}
-                    </Box>
-                    
-                    {/* Phone */}
-                    <Box>
-                        {phones.map(({phone, title}, index) => <Typography variant='p'  component="div" color="textPrimary">
-                            <FontAwesomeIcon icon={faPhone} style={{marginRight: '.25rem', opacity: index > 0 ? 0 : 1}}/> {phone} { emails.length > 1 && <Typography variant="b" component="span" color="textPrimary" style={{opacity: '.5'}}>• {title}</Typography>}
-                        </Typography>)}
-                    </Box>
-
-                    {/* Social */}
-                    <Box>
-                        {socials.map(({type, value, link, title}, index) => <Typography variant='p'  component="div" color="textPrimary">
-                            <FontAwesomeIcon icon={socialIcons[type] || socialIcons.web} style={{marginRight: '.25rem'}}/> 
-                            {title ? 
-                            <a href={value} target="_blank">{title}</a> : (
-                                link ? <a href={value} target="_blank">{value}</a> : value
-                            )} { emails.length > 1 && <Typography variant="b" component="span" color="textPrimary" style={{opacity: '.5', textTransform: 'capitalize'}}>• {type}</Typography>}
-                        </Typography>)}
-                    </Box>
-                </Stack>
-            }
-        </CardContent>
-    </Card>
+const ProfileSection = ({ title = '{{section_title}}', member, attributes = false }) => {
+	const emails = member.attributes.emails;
+	const phones = member.attributes.phones;
+	const attrs = member.attributes.attrs;
+	const socials = member.attributes.socials;
+	const socialIcons = {
+		facebook: faFacebook,
+		twitter: faTwitter,
+		telegram: faTelegram,
+		line: faLine,
+		instagram: faInstagram,
+		linkedin: faLinkedin,
+		web: faGlobe,
+	};
+	
+	return <Card variant="outlined" sx={{ height: '100%' }}>
+		<CardContent>
+			
+			<Typography variant="h6" component="div" color="textPrimary" fontWeight="600">
+				{title}
+			</Typography>
+			{
+				attributes ?
+					<Stack direction={'column'} gap={2}>
+						{attrs.map(({ name, value }, index) => <Typography variant="p"
+						                                                   component="div"
+						                                                   color="textPrimary"
+						>
+							<Box>
+								<Typography variant="b" component="b">{name}</Typography>
+								<Typography variant="body2"
+								            component="div"
+								            dangerouslySetInnerHTML={{ __html: value.split('\n').join('<br>') }}
+								></Typography>
+							</Box>
+						</Typography>)}
+					</Stack>
+					:
+					<Stack direction={'column'} gap={2}>
+						{/* Email */}
+						<Box>
+							{emails.map(({ email, title }, index) => <Typography variant="p"
+							                                                     component="div"
+							                                                     color="textPrimary"
+							>
+								<FontAwesomeIcon icon={faEnvelope}
+								                 style={{ marginRight: '.25rem', opacity: index > 0 ? 0 : 1 }}
+								/> {email} {emails.length > 1 && <Typography variant="b"
+								                                             component="span"
+								                                             color="textPrimary"
+								                                             style={{ opacity: '.5' }}
+							>• {title}</Typography>}
+							</Typography>)}
+						</Box>
+						
+						{/* Phone */}
+						<Box>
+							{phones.map(({ phone, title }, index) => <Typography variant="p"
+							                                                     component="div"
+							                                                     color="textPrimary"
+							>
+								<FontAwesomeIcon icon={faPhone}
+								                 style={{ marginRight: '.25rem', opacity: index > 0 ? 0 : 1 }}
+								/> {phone} {emails.length > 1 && <Typography variant="b"
+								                                             component="span"
+								                                             color="textPrimary"
+								                                             style={{ opacity: '.5' }}
+							>• {title}</Typography>}
+							</Typography>)}
+						</Box>
+						
+						{/* Social */}
+						<Box>
+							{socials.map(({ type, value, link, title }, index) => <Typography variant="p"
+							                                                                  component="div"
+							                                                                  color="textPrimary"
+							>
+								<FontAwesomeIcon icon={socialIcons[type] || socialIcons.web}
+								                 style={{ marginRight: '.25rem' }}
+								/>
+								{title ?
+									<a href={value} target="_blank">{title}</a> : (
+										link ? <a href={value} target="_blank">{value}</a> : value
+									)} {emails.length > 1 && <Typography variant="b"
+								                                         component="span"
+								                                         color="textPrimary"
+								                                         style={{
+									                                         opacity: '.5',
+									                                         textTransform: 'capitalize',
+								                                         }}
+							>• {type}</Typography>}
+							</Typography>)}
+						</Box>
+					</Stack>
+			}
+		</CardContent>
+	</Card>;
 };
 
 export default ProfileSection;
